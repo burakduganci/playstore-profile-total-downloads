dd7c9da926059d28b566bffc3e8bcb7d823b4dff
Burak <burak@semtr.com>
2017-12-01 14:20:55 +0300

    console logs updated

--- play-store.js
+++ play-store.js
@@ -1,44 +1,43 @@
 var Nightmare = require('nightmare'),
-  vo = require('vo');
+    vo = require('vo');
 var nightmare = Nightmare({ show: false });
 
 
 nightmare.goto("https://play.google.com/store/apps/developer?id="+process.argv[2])
-  .inject('js', 'jquery.js')
-  .wait("body")
-  .evaluate(function(){
-    var apps = [];
-    $(".card > .card-content > .card-click-target").each(function(){
-      apps.push("https://play.google.com"+ $(this).attr("href"));
-    });
-    return apps;
-  })
-  .then(function(apps){
-    console.log("Uygulamalar alındı. \n --------------");
+    .inject('js', 'jquery.js')
+    .wait("body")
+    .evaluate(function(){
+        var apps = [];
+        $(".card > .card-content > .card-click-target").each(function(){
+            apps.push("https://play.google.com"+ $(this).attr("href"));
+        });
+        return apps;
+    })
+    .then(function(apps){
+        console.log("Connection success. \n -------------- \n " + apps.length + " app(s) found.");
 
-    var run = function * () {
-    var urls = apps;
-    var links = [];
-    for (var i = 0; i < urls.length; i++) {
-        var title = yield nightmare.goto(urls[i])
-          .inject('js', 'jquery.js')
-          .wait('body')
-          .evaluate(function() {
-            var s = $('body .id-app-title').html() + " - " + $("body div[itemprop='numDownloads']").html();
-            return s;
-          })
-          .then(function(e){
+        var run = function * () {
+            var urls = apps;
+            var links = [];
+            for (var i = 0; i < urls.length; i++) {
+                var title = yield nightmare.goto(urls[i])
+                    .inject('js', 'jquery.js')
+                    .wait('body')
+                    .evaluate(function() {
+                        var s = $('body .id-app-title').html() + " - (" + $("body div[itemprop='numDownloads']").html() + ")";
+                        return s;
+                    })
+                    .then(function(e){
+                        console.log(e);
+                    })
+                    .catch((error) => {
+                    console.error('Connection failed.', error);
+            });
+            }
+            return links;
+        }
 
-            console.log(e);
-          })
-          .catch((error) => {
-            console.error('Search failed:', error);
-          });
-      }
-      return links;
-    }
-
-    vo(run)(function(err) {
-      console.log("Uygulamalar bitti.");
-    });
-  })
+        vo(run)(function(err) {
+            console.log("Process complated.");
+        });
+    })
